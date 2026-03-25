14a9b3c876f41006c3c26bebe2eaa029090e4bb7
Riddhi <riddhi.chadda@gmail.com>
2026-03-25 21:13:38 +0000

    database connection setup

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$dbname = "dg240233844_db";
+$username = "dg240233844";
+$password = "tI9Aqj6Nw9d6rCUg5OrFshcs3";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+?>
