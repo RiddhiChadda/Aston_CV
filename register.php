0799b1952b36a07e3fc4faf5f258bfaa1b7c8140
Riddhi <riddhi.chadda@gmail.com>
2026-03-27 10:03:49 +0000

    Updating after registering process

--- register.php
+++ register.php
@@ -4,6 +4,7 @@ include 'includes/functions.php';
 include 'includes/header.php';
 
 $message = "";
+$success = false;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
@@ -27,6 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result) {
         $message = "Registration successful.";
+        $success = true;
     } else {
         $message = "Something went wrong. Please try again.";
     }
@@ -35,33 +37,56 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <h2>Register</h2>
 
-<?php if ($message != ""): ?>
-    <p><?php echo $message; ?></p>
+<?php if ($success): ?>
+
+    <div style="text-align: center;">
+        <h3 style="color: green;">Registration Successful</h3>
+        <p>Your CV has been added successfully.</p>
+
+        <a href="login.php" style="
+            display: inline-block;
+            margin-top: 15px;
+            padding: 10px 20px;
+            background-color: #4b2aad;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+        ">
+            Go to Login
+        </a>
+    </div>
+
+<?php else: ?>
+
+    <?php if ($message != ""): ?>
+        <p style="color: red;"><?php echo $message; ?></p>
+    <?php endif; ?>
+
+    <form method="POST" action="">
+        <label for="name">Name</label><br>
+        <input type="text" name="name" id="name"><br><br>
+
+        <label for="email">Email</label><br>
+        <input type="email" name="email" id="email"><br><br>
+
+        <label for="password">Password</label><br>
+        <input type="text" name="password" id="password"><br><br>
+
+        <label for="keyprogramming">Key Programming Language</label><br>
+        <input type="text" name="keyprogramming" id="keyprogramming"><br><br>
+
+        <label for="profile">Profile</label><br>
+        <textarea name="profile" id="profile"></textarea><br><br>
+
+        <label for="education">Education</label><br>
+        <textarea name="education" id="education"></textarea><br><br>
+
+        <label for="URLlinks">URL Links</label><br>
+        <input type="text" name="URLlinks" id="URLlinks"><br><br>
+
+        <button type="submit">Register CV</button>
+    </form>
+
 <?php endif; ?>
-
-<form method="POST" action="">
-    <label for="name">Name</label><br>
-    <input type="text" name="name" id="name"><br><br>
-
-    <label for="email">Email</label><br>
-    <input type="email" name="email" id="email"><br><br>
-
-    <label for="password">Password</label><br>
-    <input type="text" name="password" id="password"><br><br>
-
-    <label for="keyprogramming">Key Programming Language</label><br>
-    <input type="text" name="keyprogramming" id="keyprogramming"><br><br>
-
-    <label for="profile">Profile</label><br>
-    <textarea name="profile" id="profile"></textarea><br><br>
-
-    <label for="education">Education</label><br>
-    <textarea name="education" id="education"></textarea><br><br>
-
-    <label for="URLlinks">URL Links</label><br>
-    <input type="text" name="URLlinks" id="URLlinks"><br><br>
-
-    <button type="submit">Register CV</button>
-</form>
 
 <?php include 'includes/footer.php'; ?>
