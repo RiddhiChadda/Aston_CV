2a1c17b0d83de136f076b463d52e083b550728fa
Riddhi <riddhi.chadda@gmail.com>
2026-03-27 10:56:01 +0000

    Updated login & logout states

--- includes/header.php
+++ includes/header.php
@@ -15,12 +15,23 @@ if (session_status() == PHP_SESSION_NONE) {
 <body>
     <header>
         <h1>AstonCV</h1>
+
+        <?php if (isset($_SESSION["user_name"])): ?>
+            <p>Welcome, <?php echo htmlspecialchars($_SESSION["user_name"]); ?></p>
+        <?php endif; ?>
+
         <nav>
             <a href="index.php">Home</a>
-            <a href="register.php">Register</a>
-            <a href="login.php">Login</a>
             <a href="cvs.php">View CVs</a>
             <a href="search.php">Search</a>
+
+            <?php if (isset($_SESSION["user_id"])): ?>
+                <a href="edit_cv.php">Edit My CV</a>
+                <a href="logout.php">Logout</a>
+            <?php else: ?>
+                <a href="register.php">Register</a>
+                <a href="login.php">Login</a>
+            <?php endif; ?>
         </nav>
     </header>
     <main>
